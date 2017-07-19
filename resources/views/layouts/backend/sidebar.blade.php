674013f3c4a9383df97224abf0661b4abc96fe31
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-07-19 09:45:17 +0300

    Update avatar

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -7,7 +7,7 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel">
       <div class="pull-left image">
-        <img src="#" class="img-circle" alt="User Image" />
+        <img src="{{asset('images/avatar_default.jpg')}}" class="img-circle" alt="User Image" />
       </div>
       <div class="pull-left info">
         <p>{{Auth::user()->name}}</p>
