2b42881a33056cac23530bb1d31f3bc54de3ef25
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-07-19 09:45:54 +0300

    Remove chat

--- resources/views/layouts/backend/header.blade.php
+++ resources/views/layouts/backend/header.blade.php
@@ -14,23 +14,7 @@
     <div class="navbar-custom-menu">
       <ul class="nav navbar-nav">
         <!-- Messages: style can be found in dropdown.less-->
-        <li class="dropdown messages-menu">
-          <!-- Menu toggle button -->
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <i class="fa fa-comments-o"></i>
-            <span id="unread-label" class="label label-success"></span>
-          </a>
-          <ul class="dropdown-menu">
-            <li class="header">Δημόσια Συνομιλία</li>
-            <li>
-              <!-- inner menu: contains the messages -->
-              <ul id="chat" class="menu">
-                <h4><center>Φόρτωση...</center></h4>
-              </ul><!-- /.menu -->
-            </li>
-            <li class="footer"><input id="send" class="form-control" type="text" placeholder="Πληκτρολογήστε μήνυμα" id="example-text-input"></li>
-          </ul>
-        </li><!-- /.messages-menu -->
+
 
         <!-- User Account Menu -->
         <li class="dropdown">
