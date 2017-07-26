2d7df3287a0de816ac2d3524f1143e69d77604c3
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-07-26 10:57:56 +0300

    Update footer tel

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -26,7 +26,7 @@
                             <p><i class="lnr lnr-map-marker"></i><span>1o χλμ Ιστιαίας-Αιδηψού</span></p>
                         </div>
                         <div class="info">
-                            <a href="tel:+0123456789"><i class="lnr lnr-phone-handset"></i><span>2226053649</span></a>
+                            <a href="tel:+0123456789"><i class="lnr lnr-phone-handset"></i><span>+30 22260 53649</span></a>
                         </div>
                         <div class="info">
                             <a href="mailto:baroutis.d.86@gmail.com"><i class="lnr lnr-envelope"></i><span>baroutis.d.86@gmail.com</span></a>
